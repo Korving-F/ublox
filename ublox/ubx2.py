e5684e5215673344331ea4a2ea70566b0067f8bf
korving.f@gmail.com <korving.f@gmail.com>
2017-08-31 19:35:12 +0300

    Adds NAV-DOP NAV-SOL NAV-PVT messages

--- ublox/ubx2.py
+++ ublox/ubx2.py
@@ -5,7 +5,6 @@ import struct
 import sys
 import time
 
-# Extend from object to make it a new-style class -> enables descriptors
 class UbxStream(object):
     def __init__(self, dev=None):
         
@@ -60,7 +59,6 @@ class UbxStream(object):
 
 
     def read(self, timeout=5):
-        #flush device first
         self.dev.reset_input_buffer()
         now = time.time()
         counter = 0        
@@ -83,6 +81,8 @@ class UbxStream(object):
                     ubx_id = binascii.hexlify(self.dev.read())
                     return UbxMessage(ubx_class, ubx_id, dev=self.dev)
 
+        print("Connection timed out..")
+
             
 class UbxMessage(object):
     def __init__(self, ubx_class, ubx_id, msg_type="rx", **kwargs):
@@ -92,14 +92,13 @@ class UbxMessage(object):
             #NAV
             if(ubx_class == '01'):
                 print("{} {}".format(ubx_class, ubx_id))
-                #try:
+                
                 message = {'02': lambda: self.__ubx_NAV_POSLLH(kwargs["dev"]),
                            '04': lambda: self.__ubx_NAV_DOP(kwargs["dev"]),
                            '06': lambda: self.__ubx_NAV_SOL(kwargs["dev"]),
                            '07': lambda: self.__ubx_NAV_PVT(kwargs["dev"])}
                 message[ubx_id]()
-               # except NameError:
-                #    print("Serial device was not passed through.")
+
             #RXM
             elif(ubx_class == '02'):
                 print("")
@@ -148,11 +147,11 @@ class UbxMessage(object):
             print("Transmitting")
             
 
+    ## UBX-NAV 0x01 ##
 
     # time_of_week in ms / longitude in deg / latitude in deg
     # height ellipsoid in mm / height mean sea level mm
     # horizontal accuracy in mm / vertical accuracy in mm
-
     def __ubx_NAV_POSLLH(self, dev):
         payload = dev.read(size=30)
         if self.__calc_checksum(1, 2, payload, dev):
@@ -165,6 +164,63 @@ class UbxMessage(object):
                 print("{} {}".format(sys.exc_info()[0], sys.exc_info()[1]))
 
     
+    # time_of_week in ms / Dilution of Precision
+    # DOP is Dimensionless / scaled by factor 100
+    # Geometric / Position / Time / Vertical / Horizontal / Northing / Easting
+    def __ubx_NAV_DOP(self, dev):
+        payload = dev.read(size=20)
+        if self.__calc_checksum(1, 4, payload, dev):
+            try:
+                payload = payload[2:]
+                self.iTOW, self.gDOP, self.pDOP, self.tDOP, self.vDOP, self.hDOP, self.nDOP, self.eDOP = struct.unpack('=L7H', payload)
+                
+            except struct.error:
+                print("{} {}".format(sys.exc_info()[0], sys.exc_info()[1]))
+
+                
+    # Time_of_week in ms / Fractional time_of_week ns / Week number
+    # GPS Fix (6 valid types depending on status) / Fix status flags (4 types)
+    # ECEF X cm / ECEF Y cm / ECEF Z cm / Position Accuracy cm
+    # ECEF-Velocity X cm/s / ECEF-Velocity Y cm/s / ECEF-Velocity Z cm/s
+    # Speed Accuracy cm/s / Position DOP (scale 0.01)
+    # reserved / number of SV's used / reserved
+    def __ubx_NAV_SOL(self, dev):
+        payload = dev.read(size=54)
+        if(self.__calc_checksum(1, 6, payload, dev)):
+            try:
+                payload = payload[2:]
+                self.iTOW, self.fTOW, self.week, self.gpsFix, self.flags, self.ecefX, self.ecefY, self.ecefZ, self.pAcc, self.ecefVX, self.ecefVY, self.ecefVZ, self.sAcc, self.pDOP, reserved1, self.numSV, reserved21, reserved22, reserved23, reserved24 = struct.unpack('=LlhBB3lL3lLH6B', payload)
+                
+            except struct.error:
+                print("{} {}".format(sys.exc_info()[0], sys.exc_info()[1]))
+
+
+
+    # Time of week in ms / Year(UTC) / Month (UTC 1..12) / Day (1..31)
+    # Hour (0..23) / Minute (0..59) / Seconds (0..60) / Validity Flags
+    # Time Accuracy Estimate in ns / Fraction of second (-1e9..1e9)
+    # GNSS Fix Type / Fix Status Flags / Other Flags / Number of Sattelites
+    # Longitude in deg (1e-7) / Latitude in deg (1e-7) / Height Ellipsoid in mm
+    # Height Sea Level in mm / Horizontal Accuracy in mm / Vertical Accuracy in mm
+    # North Velocity mm s^-1 / East Velocity mm s^-1 / Down Velocity mm s^-1
+    # Ground speed in mm s^-1 / Heading of Motion in deg (1e-5) / Speed accuracy mm s^-1
+    # Heading accuracy estimate deg (1e-5) / Position DOP (0.01) / Reserved space
+    # Heading of Vehicle in deg (1e-5) / Magnetic Declination in deg (1e-2) <-+ Accuracy in deg
+    def __ubx_NAV_PVT(self, dev):
+        payload = dev.read(size=94)
+        if(self.__calc_checksum(1, 7, payload, dev)):
+            try:
+                payload = payload[2:]
+                self.iTOW, self.year, self.month, self.day, self.hour, self.minute, self.second, self.valid, self.tAcc, self.nano, self.fixType, self.flags, self.flags2, self.numSV, self.lon, self.lat, self.height, self.hMSL, self.hAcc, self.vAcc, self.velN, self.velE, self.velD, self.gSpeed, self.headMot, self.sAcc, self.headAcc, self.pDOP, reserved11, reserved12, reserved13, reserved14, reserved15, reserved16,  self.headVeh, self.magDec, self.magAcc = struct.unpack('=LH5BBLlB2BB4l2L5lLLH6BlhH', payload)
+                
+            except struct.error:
+                print("{} {}".format(sys.exc_info()[0], sys.exc_info()[1]))
+
+            
+
+    ## UBX-CFG 0x06 ##
+
+
     
     # Checksum is calculated over class/id/length/payload of packet
     # using 8-bit Fletcher Algorithm (also used in TCP) RFC 1145
